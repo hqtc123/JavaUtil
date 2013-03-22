a5fc345ee7d130457d03f22518a1544ef0663ab6
jabbby <814216440@qq.com>
2013-03-22 21:56:07 +0800

    init

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: hqtc
+ * Date: 13-3-22
+ * Time: 下午9:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test{
+    public static void main(String[] args){
+        Integer integer=new Integer(9);
+        System.out.println("hello");
+    }
+}
---
+++ src/sort/QuickSort.java
@@ -0,0 +1,11 @@
+package sort;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hqtc
+ * Date: 13-3-22
+ * Time: 下午9:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class QuickSort {
+}
